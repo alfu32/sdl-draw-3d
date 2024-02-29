793fa958cfe56b3dee824f36b2468a1485fcffc8
alferio <ioan-cristian.alferaru@ec.europa.eu>
2024-02-29 07:25:55 +0100

    feat(orbit):

--- raylib-app.c
+++ raylib-app.c
@@ -38,15 +38,25 @@ int main(void) {
     Vector2 mouse_init;
     Vector2 mouse_orbit;
     char start_orbit=0;
+    double azimuth_0;
+    double elevation_0;
     while (!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_FREE );// Update camera position/movement
 
         if (IsKeyPressed('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        if (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT)){
+            Ray ray = GetMouseRay(GetMousePosition(), camera);
+            cursor.x=ray.direction.x*10;
+            cursor.y=ray.direction.y*10;
+            cursor.z=ray.direction.z*10;
+            //GetRayCollisionMesh
+        }
         if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
             pos_init=(Vector3){camera.position.x,camera.position.y,camera.position.z};
             mouse_init=GetMousePosition();
             start_orbit=1;
-
+            azimuth_0 = ((mouse_orbit.x - mouse_init.x) / screenWidth) * 2 * PI;
+            elevation_0 = ((mouse_orbit.y - mouse_init.y) / screenHeight) * PI / 2;
         }
         if (start_orbit && IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
             float orbitRadius = Vector3Distance(camera.position, camera.target);
@@ -55,11 +65,11 @@ int main(void) {
             double elevation = ((mouse_orbit.y - mouse_init.y) / screenHeight) * PI / 2; // PI / 2 for vertical rotation within reasonable limits
 
             // Horizontal orbit (azimuth)
-            camera.position.x = camera.target.x + cos(azimuth) * orbitRadius * cos(elevation);
-            camera.position.z = camera.target.z + sin(azimuth) * orbitRadius * cos(elevation);
+            camera.position.x = camera.target.x + cos(azimuth-azimuth_0) * orbitRadius * cos(elevation-elevation_0);
+            camera.position.z = camera.target.z + sin(azimuth-azimuth_0) * orbitRadius * cos(elevation-elevation_0);
 
             // Vertical orbit (elevation)
-            camera.position.y = camera.target.y + sin(elevation) * orbitRadius;
+            camera.position.y = camera.target.y + sin(elevation-elevation_0) * orbitRadius;
         }
         if (start_orbit && IsMouseButtonReleased(MOUSE_BUTTON_RIGHT)){
             start_orbit=0;
@@ -72,6 +82,7 @@ int main(void) {
 
         // Render the scene
         scene__render0(&myScene);
+        voxel__render0(&cursor);
         DrawGrid(10, 1.0f);
 
         // Optionally, to cast a ray from the mouse:
