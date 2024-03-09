4bac364cdecceedaefb0c1862f71964c5693c295
Ioan Alferaru <ioan.alferaru@gmail.com>
2024-03-09 16:14:29 +0100

    Update _build.ps1

--- _build.ps1
+++ _build.ps1
@@ -1,32 +1,42 @@
 #!/usr/bin/pwsh
 
-$appname = "vxdi-app-main"
-$flags = "& {sdl2-config --libs --cflags} -lraylib -lGL -lGLU -lm -lpthread -ldl -lrt -lSDL2_image -lSDL2_ttf"
-$zig_flags = "-std=gnu99 -D_GNU_SOURCE -DGL_SILENCE_DEPRECATION=199309L -fno-sanitize=undefined"
-Remove-Item -Path "build/$appname.exe" -ErrorAction SilentlyContinue
+# Define base libraries URLs
+$sdl2Url = "https://www.libsdl.org/release/SDL2-devel-2.0.x-VC.zip"
+$sdl2TtfUrl = "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.x-VC.zip"
+$sdl2ImageUrl = "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.x-VC.zip"
+$raylibUrl = "https://github.com/raysan5/raylib/releases/download/3.7.0/raylib-3.7.0_win64_msvc15_lib.zip"
 
-if (Test-Path "external/raygui.h") {
-    Write-Output "rayguy.h exists"
-} else {
-    Invoke-WebRequest "https://raw.githubusercontent.com/raysan5/raygui/master/src/raygui.h" -OutFile "external/raygui.h"
+# Define header-only libraries URLs
+$nuklearUrl = "https://github.com/Immediate-Mode-UI/Nuklear/archive/master.zip"
+$rayguiUrl = "https://github.com/raysan5/raygui/archive/master.zip"
+
+# Create directories for the libraries
+$libsDir = "libs"
+$sdl2Dir = Join-Path $libsDir "SDL2"
+$sdl2TtfDir = Join-Path $libsDir "SDL2_ttf"
+$sdl2ImageDir = Join-Path $libsDir "SDL2_image"
+$raylibDir = Join-Path $libsDir "Raylib"
+$nuklearDir = Join-Path $libsDir "Nuklear"
+$rayguiDir = Join-Path $libsDir "raygui"
+
+New-Item -ItemType Directory -Force -Path $sdl2Dir, $sdl2TtfDir, $sdl2ImageDir, $raylibDir, $nuklearDir, $rayguiDir
+
+# Function to download and extract libraries
+Function DownloadAndExtract ($url, $destination) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $downloadPath = Join-Path $libsDir $fileName
+    Invoke-WebRequest $url -OutFile $downloadPath
+    Expand-Archive $downloadPath -DestinationPath $destination -Force
+    Remove-Item $downloadPath
 }
 
-if (Test-Path "external/nuklear.h") {
-    Write-Output "nuklear.h exists"
-} else {
-    Invoke-WebRequest "https://raw.githubusercontent.com/Immediate-Mode-UI/Nuklear/master/nuklear.h" -OutFile "external/nuklear.h"
-}
+# Download and extract the libraries
+Write-Host "Downloading and extracting SDL2, SDL2_ttf, SDL2_image, Raylib, Nuklear, and raygui..."
+DownloadAndExtract $sdl2Url $sdl2Dir
+DownloadAndExtract $sdl2TtfUrl $sdl2TtfDir
+DownloadAndExtract $sdl2ImageUrl $sdl2ImageDir
+DownloadAndExtract $raylibUrl $raylibDir
+DownloadAndExtract $nuklearUrl $nuklearDir
+DownloadAndExtract $rayguiUrl $rayguiDir
 
-if (Test-Path "external/raylib-nuklear.h") {
-    Write-Output "raylib-nuklear.h exists"
-} else {
-    Invoke-WebRequest "https://raw.githubusercontent.com/RobLoach/raylib-nuklear/master/include/raylib-nuklear.h" -OutFile "external/raylib-nuklear.h"
-}
-
-Write-Output "zig cc -o build/$appname.xe $appname.c $flags $zig_flags"
-& zig cc -o "build/$appname.xe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args
-#& zig cc -o "$appname.xe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args -target "aarch64-linux-gnu"
-#& zig cc -o "$appname.xe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args -target "x86_64-linux-gnu"
-#& zig cc -o "$appname.xe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args -target "x86_64-linux-musl"
-#& zig cc -o "$appname.xe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args -target "riscv64-linux-gnu"
-& zig cc -o "$appname.exe" "$appname.c" $flags $zig_flags -I"/usr/include/SDL2" $args -target "x86_64-windows-msvc"
+Write-Host "Libraries downloaded and extracted."
