d310170947e32edbcd3980afdb865b6c6d068ca4
alfu32 <ioan.alferaru@gmail.com>
2024-03-10 05:16:51 +0100

    feat(crossbuild): fix opengl opengles

--- prepare-deps.win64-mingw.sh
+++ prepare-deps.win64-mingw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+openglESMesaUrl="https://github.com/pal1000/mesa-dist-win/releases/download/24.0.2/mesa3d-24.0.2-release-mingw.7z"
 # Define base libraries URLs
 sdl2Url="https://www.libsdl.org/release/SDL2-devel-2.30.1-mingw.zip"
 sdl2TtfUrl="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.22.0-mingw.zip"
@@ -12,6 +12,7 @@ rayguiUrl="https://github.com/raysan5/raygui/archive/master.zip"
 
 # Create directories for the libraries
 libsDir="libs"
+openglESMesaDir="${libsDir}"
 sdl2Dir="${libsDir}"
 sdl2TtfDir="${libsDir}"
 sdl2ImageDir="${libsDir}"
@@ -36,16 +37,20 @@ downloadAndExtract() {
 
 # Download and extract the libraries
 echo "Downloading and extracting SDL2, SDL2_ttf, SDL2_image, Raylib, Nuklear, and raygui..."
+mkdir -p libs/mesa
+wget "https://github.com/pal1000/mesa-dist-win/releases/download/24.0.2/mesa3d-24.0.2-development-pack-mingw.7z" -O "libs/mesa/mesa3d-24.0.2-development-pack-mingw.7z"
+cd libs/mesa
+p7zip -d mesa3d-24.0.2-development-pack-mingw.7z
+
 downloadAndExtract "$sdl2Url" "$sdl2Dir"
 downloadAndExtract "$sdl2TtfUrl" "$sdl2TtfDir"
 downloadAndExtract "$sdl2ImageUrl" "$sdl2ImageDir"
+mv "${libsDir}/SDL2-2.30.1" "${libsDir}/SDL2"
+mv "${libsDir}/SDL2_image-2.8.2" "${libsDir}/SDL2_image"
+mv "${libsDir}/SDL2_ttf-2.22.0" "${libsDir}/SDL2_ttf"
 
 downloadAndExtract "$raylibUrl" "$raylibDir"
 downloadAndExtract "$nuklearUrl" "$nuklearDir"
 downloadAndExtract "$rayguiUrl" "$rayguiDir"
-
-## mv "${libsDir}/SDL2-2.30.1" "${libsDir}/SDL2"
-## mv "${libsDir}/SDL2_image-2.8.2" "${libsDir}/SDL2_image"
-## mv "${libsDir}/SDL2_ttf-2.22.0" "${libsDir}/SDL2_ttf"
 
 echo "Libraries downloaded and extracted."
