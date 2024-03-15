834c86b6bd6d8d330b8f72fe11dd5f5445da6c3a
alfu32 <ioan.alferaru@gmail.com>
2024-03-15 19:08:44 +0100

    feat(build): win archive, linux archive.

--- build-linux.sh
+++ build-linux.sh
@@ -4,3 +4,4 @@
 make
 rm -f build/linux64/voxd31
 mv voxd31 build/linux64/
+tar -czvf build/voxd31-x86_64-linux.tar.gz build/linux64
--- build-mingw.cmd
+++ build-mingw.cmd
@@ -1,3 +1,5 @@
 
 rm build/win64/voxd31.exe
 gcc -o build/win64/voxd31.exe main.c -Wall -std=c99 -D_DEFAULT_SOURCE -Wno-missing-braces -s -I. -IF:/Development/raylib/src/external -L. -LF:/Development/c99-raylib-vox/lib/raylib-5.0_win64_mingw-w64/lib -IF:/Development/c99-raylib-vox/lib/raylib-5.0_win64_mingw-w64/include -lraylib -lopengl32 -lgdi32 -lwinmm -DPLATFORM_DESKTOP
+
+7z a voxd31-x86_64-windows ./build/win64/*
